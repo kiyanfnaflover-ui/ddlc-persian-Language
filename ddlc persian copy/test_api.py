1c9a50414362e8df0000589d8c3140aadfdf49bc
K_F_ <kiyanfnaflover@gmail.com>
2026-07-25 02:51:42 +0330

    Delete ddlc persian copy directory

--- ddlc persian copy/test_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Encoding
